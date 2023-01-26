91945a4c0bdf2d339f5a71e9efd92f688e966614
Dagno Lopes <dagno.lopes2019@gmail.com>
2023-01-25 22:45:06 -0300

    projeto local-storage

--- src/pages/Contexto/styles.ts
+++ src/pages/Contexto/styles.ts
@@ -3,10 +3,8 @@ import styled from "styled-components";
 export const Grupo = styled.h1`
   flex: 1;
   padding: 3.5rem;
-
   display: flex;
   flex-direction: column;
-
   h1 {
     font-size: 1.5rem;
     color: ${(props) => props.theme["gray-100"]};
